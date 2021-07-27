e284e9c2f5b70edabedbb25bc5ed512a6b18a8f9
lucamartinatti <luca.martinatti@gmail.com>
2021-07-27 11:18:52 +0200

    Dataset loading and first pre-processing phase

--- main.py
+++ main.py
@@ -6,20 +6,65 @@ Version 0.0
 
 '''
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow._api.v2 import data
+from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.preprocessing.text import Tokenizer
-
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.python.keras.backend import concatenate
+from tensorflow.python.ops.gen_array_ops import shape
 
 
 if __name__=="__main__":
 
     # Input and parameters
-    N = 100
-    phrases_to_process = [ 'The pen is on the table', 'The pen is under the table', 'Over the table there is a pen']
+    vocab_size = 10000
+    embedding_dim = 16
+    max_length = 100
+    trunc_type='post'
+    padding_type='post'
+    oov_tok = "<OOV>"
+    training_perc = 90
 
-    # Create an instance of the tokenizer
-    tokenizer = Tokenizer(num_words=N)
-    tokenizer.fit_on_texts(phrases_to_process)
-    indices = tokenizer.word_index
-    print(indices)
+    # Load and pre-process the dataset
+    df_ture = pd.read_csv('./archive/True.csv')
+    df_fake = pd.read_csv('./archive/Fake.csv')
+
+    #TODO normalize text value and check possible missing values
+
+    # Add lables
+    df_ture['Fake'] = [0]*df_ture.shape[0]
+    df_fake['Fake'] = [1]*df_fake.shape[0]
+
+    # Join the two datasets
+    dataset = pd.concat([df_ture, df_fake], axis=0)
+    
+    # Shuffle the DataFrame rows
+    dataset = dataset.sample(frac = 1)
+
+    # Create training and testing dataset
+    val = round(dataset.shape[0]*training_perc/100)
+    train_dataset = dataset[:val]
+    test_dataset = dataset[val:]
+
+    # Tokenization
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+    tokenizer.fit_on_texts(train_dataset)
+
+    word_index = tokenizer.word_index
+
+    training_sequences = tokenizer.texts_to_sequences(train_dataset['title'])
+    training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+    testing_sequences = tokenizer.texts_to_sequences(test_dataset['title'])
+    testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+
+   
+    
+ 
